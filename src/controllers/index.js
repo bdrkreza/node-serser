4e38b46ab6ea37559a4068249dc2f0ca36595a47
Md Rezaul Karim <73929234+bdrkreza@users.noreply.github.com>
2022-02-09 00:37:32 +0600

    order route update

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,5 +3,6 @@ module.exports = {
   productCtrl: require("./product.controller"),
   categoriesCtrl: require("./categories.controller"),
   orderCtrl: require("./order.controller"),
-  cartCtrl: require("./cart.controller")
+  cartCtrl: require("./cart.controller"),
+  stripe:require("./stripe.controller")
 };
